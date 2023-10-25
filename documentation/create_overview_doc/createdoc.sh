5e5b21b5103132e499610947feee5e9a7f3fd330
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-10-24 22:04:35 -0700

    Big Update - AI Language Agnostic, with filtering what to be documented, real time logging of usage, and a much simpler Object Oriented Design to work with for developers to modify.
    Enhanced Features:
    
    Selective Recursive Documentation:
    
    The script now recursively traverses the provided directory and its subdirectories to generate documentation.
    Leveraging a predefined list, it can selectively skip specific files and folders, ensuring that only relevant modules are documented.
    The final output will be High_Level_Design.md inside a dedicated documentation directory (doc_<folder_to_document>), offering a detailed overview of the entire system.
    Intelligent Docstring Extraction:
    
    The enhanced script utilizes the bito tool to intelligently aggregate high-level docstrings from modules.
    While it is primarily geared towards Python and JavaScript files, the script's modular design allows for easy expansion to other file types in the future.
    For each processed module, word and character counts are logged for transparency.
    Code Flow Visualization:
    
    The script now integrates the code2flow tool to visualize the code flow of the modules.
    Flow maps are generated for both Python and JavaScript files, providing a graphical representation of the code's structure.
    These visualizations are embedded directly into the final High_Level_Design.md, ensuring that users can access both textual and visual information in one consolidated document.
    Robust Verification and Logging:
    
    Before generating documentation, the script checks for the availability of all required tools (bito, code2flow, dot) and essential prompt files.
    Any missing elements result in a clear error message, guiding the user on necessary installations or file placements.
    The script also includes a logging mechanism (log_word_char_count), allowing users to track the word and character counts of the aggregated docstrings.
    Note: Ensure all required dependencies are installed and that the essential prompt files are placed in the specified prompt_folder before running the script.

--- documentation/create_overview_doc/createdoc.sh
+++ documentation/create_overview_doc/createdoc.sh
@@ -1,32 +1,49 @@
 #!/bin/bash
 
-PYTHON_FILES="*.py"
-JS_FILES="*.js"
+log_file="bito_usage_log.txt"
+prompt_folder="AI_Prompts"  # Specify the prompt folder here
 
-check_tools() {
-  local tools=("bito" "code2flow" "dot" "jq")
-  local missing_tools=()
+function log_word_char_count() {
+    local module_name="$1"
+    local count_type="$2"
+    local word_count="$3"
+    local char_count="$4"
+    local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+    echo "$timestamp - Module '$module_name' Count $count_type BitO CLI: Words: $word_count, Chars: $char_count" | tee -a "$log_file"
+}
 
-  for tool in "${tools[@]}"; do
-    if ! command -v "$tool" &> /dev/null; then
-      missing_tools+=("$tool")
-    fi
-  done
+function check_tools_and_files() {
+    local required_tools=("bito" "code2flow" "dot")
+    local missing_tools=()
 
-  if [ ${#missing_tools[@]} -ne 0 ]; then
-    echo "Error: The following tools are required but not installed:"
-    for tool in "${missing_tools[@]}"; do
-      echo " - $tool"
+    for tool in "${required_tools[@]}"; do
+        if ! command -v "$tool" &> /dev/null; then
+            missing_tools+=("$tool")
+        fi
     done
-    echo "Exiting."
-    exit 1
-  fi
+
+    local required_files=("high_level_docstrings_prompt.txt" "system_summary_prompt.txt" "refined_organized_markdown_prompt.txt" )
+    for file in "${required_files[@]}"; do
+        if [ ! -f "$prompt_folder/$file" ]; then
+            echo "Error: Missing required file: $prompt_folder/$file"
+            exit 1
+        fi
+    done
+
+    if [ ${#missing_tools[@]} -ne 0 ]; then
+        echo "Error: The following tools are required but not installed:"
+        for missing_tool in "${missing_tools[@]}"; do
+            echo " - $missing_tool"
+        done
+        echo "Exiting."
+        exit 1
+    fi
 }
 
 is_skippable() {
   local path=$1
   
-  local skip_dirs_files=("logs" "node_modules" "dist" "target" "bin" "package-lock.json" "data.json" "build" ".gradle" ".idea" "gradle" "extension.js" "vendor.js" "ngsw.json" "polyfills.js")
+  local skip_dirs_files=("logs" "node_modules" "dist" "target" "bin" "package-lock.json" "data.json" "build" ".gradle" ".idea" "gradle" "extension.js" "vendor.js" "ngsw.json" "polyfills.js" "init")
 
   for skip_item in "${skip_dirs_files[@]}"; do
     if [[ "$path" == *"$skip_item"* ]]; then
@@ -41,218 +58,205 @@ is_skippable() {
   return 1
 }
 
-copy_dir_structure() {
-  local src=$1
-  local dest="doc_$2" 
-
-  echo -e "\n---- Copying directory structure from $src to $dest ----"
-  find "$src" -mindepth 1 -type d | sed "s|^$src/||" | while read -r dir; do
-    if [ -d "$src/$dir" ] && ! is_skippable "$src/$dir"; then
-      mkdir -p "$dest/$dir"
+function aggregate_module_high_level_docstrings() {
+    local module_path="$1"
+    
+    if is_skippable "$module_path"; then
+        echo "Skipped $module_path as it's in the skippable list."
+        return
     fi
-  done
+    
+    local module_name=$(basename "$module_path")
+    local module_contents
+
+    module_contents=$(<"$module_path")
+
+    local high_level_docstrings=$(echo -e "Module: $module_name\n---\n$module_contents" | bito -p "$prompt_folder/high_level_docstrings_prompt.txt")
+
+    if [ $? -eq 0 ]; then
+        echo "$high_level_docstrings"
+    else
+        echo "Bito operation failed for module: $module_name"
+        return 1
+    fi
 }
 
-generate_flow_docs() {
-  local module=$1
-  local flow_doc_file=$2
-  local lang_option=$3
+function generate_system_overview() {
+    local aggregated_docstrings_file="$1"
+    local aggregated_docstrings=$(cat "$aggregated_docstrings_file")
+    local system_overview=$(echo -e "Aggregated Docstrings:\n---\n$aggregated_docstrings" | bito -p "$prompt_folder/system_summary_prompt.txt")
 
-  echo -e "\n---- Generating code flow docs for files in $module using language: $lang_option ----"
+    if [ $? -eq 0 ]; then
+        echo "$system_overview"
+    else
+        echo "Bito operation failed while generating system overview"
+        return 1
+    fi
+}
 
-  if [[ -d "$module" ]]; then
-    code2flow --output "$flow_doc_file" --language "$lang_option" "$module"/*.py "$module"/*.js --quiet
-  elif [[ -f "$module" ]]; then
-    code2flow --output "$flow_doc_file" --language "$lang_option" "$module" --quiet
-  fi
+function create_combined_context() {
+    local aggregated_module_docstrings_file="$1"
+    local system_overview_file="$2"
+    local combined_context_file="$docs_folder/combined_context.txt"
+    
+    # Use 'cat' to concatenate files efficiently
+    cat "$aggregated_module_docstrings_file" "$system_overview_file" > "$combined_context_file"
+
+    echo "Combined context generated and saved in combined_context.txt"
+}
+
+function generate_refined_design_doc() {
+    local input_file="$docs_folder/combined_context.txt"
+    local output_file="$docs_folder/High_Level_Design.md"
+
+    # Read the content of combined_context.txt
+    local combined_content=$(<"$input_file")
+
+    # Read prompt
+    local prompt=$(<"$prompt_folder/refined_organized_markdown_prompt.txt")
+
+    # Prepend the content of combined_context.txt to the prompt
+    local combined_prompt="${combined_content}\n${prompt}"
+
+    # Call BitO with combined prompt
+    local refined_doc=$(echo -e "$combined_prompt" | bito)
+
+    # Check if bito succeeded or failed
+    if [ $? -ne 0 ]; then
+        echo "Bito operation failed while generating refined design doc"
+        return 1
+    fi
+
+    # Write refined doc to output file
+    echo "$refined_doc" > "$output_file"
 }
 
 generate_flow_map() {
-  local module=$1
-  local flow_map_file=$2
-  local lang_option=$3
+    local module=$1
+    local flow_map_file=$2
+    local lang_option=$3
 
-  echo -e "\n---- Generating code flow map for files in $module using language: $lang_option ----"
+    echo -e "\n---- Generating code flow map for files in $module using language: $lang_option ----"
 
-  if [[ -d "$module" ]]; then
-    code2flow --output "$flow_map_file" --language "$lang_option" "$module"/*.py "$module"/*.js --quiet
-  elif [[ -f "$module" ]]; then
-    code2flow --output "$flow_map_file" --language "$lang_option" "$module" --quiet
-  fi
-}
+    local file_extension
+    case $lang_option in
+        "py") file_extension="*.py" ;;
+        "js") file_extension="*.js" ;;
+        # Add other cases as needed
+        *) file_extension="*.*" ;;  # Default
+    esac
 
-extract_python_docstrings() {
-  awk '/^\s*\"\"\"/ {if (in_doc) {print "```"; in_doc=0} else {print "```python"; in_doc=1}} in_doc' "$1"
-}
+    local files_to_process=($(find "$module" -type f -name "$file_extension"))
 
-extract_js_docstrings() {
-  awk '/^\s*(\/\*\*|\/\/\*\*)/ {if (in_doc) {print "```"; in_doc=0} else {print "```js"; in_doc=1}} in_doc' "$1"
-}
+    # Debugging step to print the files you're about to process
+    echo "Files to process with code2flow: ${files_to_process[@]}"
 
-extract_docstrings() {
-  if [[ "$1" == $PYTHON_FILES ]]; then
-    extract_python_docstrings "$1" 
-  elif [[ "$1" == $JS_FILES ]]; then 
-    extract_js_docstrings "$1"
-  fi
-}
-
-generate_file_summary() {
-  local file=$1
-  local prompt_file=$2
-  local file_summary=$(cat "$file" | bito -p "$prompt_file")
-  echo "File: $file"
-  echo "Bito Output: $file_summary"
-}
-
-aggregate_file_summaries() {
-  local module=$1
-  local prompt_file=$2
-  local summaries=""
-  find "$module" -type f \( -name $PYTHON_FILES -o -name $JS_FILES \) | while read file; do
-    if ! is_skippable "$file"; then
-      file_summary=$(generate_file_summary "$file" "$prompt_file")
-      summaries+="### $(basename "${file%.*}")\n\n$file_summary\n\n---\n"
+    if [[ ${#files_to_process[@]} -eq 0 ]]; then
+        echo "Warning: No files found for the language: $lang_option in $module"
+        return 1
     fi
-  done
-  echo "$summaries"
+
+    code2flow --output "$flow_map_file" --language "$lang_option" "${files_to_process[@]}" --quiet
 }
 
-generate_module_overview() {
-  local module=$1
-  local doc_folder=$2
+function main() {
+    check_tools_and_files
 
-  if is_skippable "$module"; then
-    return
-  fi
-
-  echo -e "\n---- Generating module overview for $module ----"
-
-  local docstrings=$(extract_docstrings "$module")
-  local module_name=$(basename "$module")
-
-  local overview_file="$doc_folder/overview.md"
-  echo "## $module_name Overview" > "$overview_file"
-  echo -e "$docstrings" >> "$overview_file"
-
-  local summaries=$(aggregate_file_summaries "$module" "docprmt.txt")
-  echo -e "$summaries" >> "$overview_file"
-
-  local has_python_files=$(find "$module" -type f -name "*.py" 2>/dev/null | wc -l)
-  local has_js_files=$(find "$module" -type f -name "*.js" 2>/dev/null | wc -l)
-  local lang_option=""
-
-  if [ $has_python_files -gt 0 ] && [ $has_js_files -eq 0 ]; then
-    lang_option="py"
-  elif [ $has_js_files -gt 0 ] && [ $has_python_files -eq 0 ]; then
-    lang_option="js"
-  elif [ $has_python_files -gt 0 ] && [ $has_js_files -gt 0 ]; then
-    echo "Warning: Both Python and JavaScript files detected in $module. Defaulting to Python for code2flow."
-    lang_option="py"
-  else
-    echo "No Python or JavaScript files found in $module. Skipping flow map generation."
-    return
-  fi
-
-  local json_output_file="$doc_folder/code2flow_output.json"
-  generate_json_output "$module" "$json_output_file" "$lang_option"
-
-  local has_nodes=$(jq '.graph.nodes | length' "$json_output_file")
-  local has_edges=$(jq '.graph.edges | length' "$json_output_file")
-
-  if [ "$has_nodes" -eq 0 ] && [ "$has_edges" -eq 0 ]; then
-    echo "No code flow detected for $module. Skipping code flow documentation."
-    rm "$json_output_file"
-    return
-  fi
-
-  local documentation=$(bito --file "$json_output_file" -p overviewprmt.txt)
-  echo -e "\n## Code Flow Documentation\n\n$documentation\n" >> "$overview_file"
-
-  local flow_map_file="$doc_folder/flow_map.png"
-  generate_flow_map "$module" "$flow_map_file" "$lang_option"
-  echo -e "\n## Flow Map\n\n![Flow Map](/$flow_map_file)\n" >> "$overview_file"
-}
-
-# Function to generate codebase overview
-generate_codebase_overview() {
-  # Start the codebase overview markdown content.
-  local codebase_overview="## Codebase Overview\n\n"
-  # Loop through each module in the codebase.
-  for module in $modules; do
-    # Check and skip hidden directories as they're typically not part of the main code.
-    if is_skippable "$module"; then
-      continue
+    if [ $# -eq 0 ]; then
+        echo "Please provide a folder name as a command line argument"
+        exit 1
     fi
+
+    folder_to_document="$1"
+    # docs_folder="$folder_to_document/doc_$folder_to_document"
+    docs_folder="doc_"$(basename "$folder_to_document")
     
-    # Extract the module's name.
-    local module_name=$(basename "$module")
-    # Append the overview of the current module to the codebase overview.
-    local module_overview_file="doc_$module/overview.md"
-    if [ -f "$module_overview_file" ]; then
-      codebase_overview+="### $module_name\n\n$(cat "$module_overview_file")\n\n---\n"
-    else
-      echo "No overview file found for $module_name"
+    if [ ! -d "$folder_to_document" ]; then
+        echo "Folder $folder_to_document does not exist"
+        exit 1
     fi
-  done
 
-  echo -e "\n---- Generating codebase flow map ----"
+    if [ ! -d "$docs_folder" ]; then
+        mkdir "$docs_folder"
+    fi
 
-  local codebase_flow_map_file="doc_$folder/codebase_flow_map.png"
-  if [[ -d "$folder" ]]; then
-    code2flow --output "$codebase_flow_map_file" "$folder" --quiet
-  fi
-  codebase_overview+="## Codebase Flow Map\n\n![Codebase Flow Map](/$codebase_flow_map_file)\n\n"
-  local high_level_summary=$(echo -e "$codebase_overview" | bito -p high_level_prompt.txt)
-  local final_content="Summary: $high_level_summary\n\n$codebase_overview"
-  local filename=${folder##*/} 
-  local overview_file="doc_$filename/codebase_overview.md"
-  echo -e "---- Writing codebase overview to $overview_file ----"
-  echo -e "$final_content" | sed '/## Flow Map/d' > "$overview_file"
+    aggregated_module_docstrings_file="$docs_folder/aggregated_module_docstrings.txt"
+    touch "$aggregated_module_docstrings_file"
+    
+    # For Python, C, C++, Java, JavaScript, Go, Rust    
+    module_files=$(find "$folder_to_document" -type f \( -name '*.py' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.js' -o -name '*.go' -o -name '*.rs' \))
+
+    total_modules=0
+    for module_file in $module_files; do
+        if ! is_skippable "$module_file"; then
+            ((total_modules++))
+        fi
+    done
+
+    aggregated_module_count=0
+    running_word_count=0
+    running_char_count=0
+    divider="=========================================================="
+
+    for module_file in $module_files; do
+        if is_skippable "$module_file"; then
+            continue  # Skip the current iteration and move to the next file
+        fi
+
+        module_summary=$(aggregate_module_high_level_docstrings "$module_file")
+
+        if [ $? -ne 0 ]; then
+            echo "Exiting due to Bito operation failure."
+            exit 1
+        fi
+
+        ((aggregated_module_count++))
+
+        word_count=$(echo -e "$module_summary" | wc -w)
+        char_count=$(echo -n "$module_summary" | wc -c)
+
+        module_filename=$(basename "$module_file")
+
+        running_word_count=$((running_word_count + word_count))
+        running_char_count=$((running_char_count + char_count))
+
+        echo "$divider"
+        echo "Aggregated '$module_filename' (Words: $word_count, Chars: $char_count)"
+        echo "$aggregated_module_count out of $total_modules modules"
+        echo "Total Word Count: $running_word_count, Total Char Count: $running_char_count"
+        echo "$divider"
+
+        echo -e "$module_summary\n" >> "$aggregated_module_docstrings_file"
+    done
+
+    # [The rest of your function remains unchanged.]
+
+    system_overview_file="$docs_folder/system_overview.txt"
+    system_overview=$(generate_system_overview "$aggregated_module_docstrings_file")
+    echo "$system_overview" > "$system_overview_file"
+
+    create_combined_context "$aggregated_module_docstrings_file" "$system_overview_file"
+
+    generate_refined_design_doc "$docs_folder/combined_context.txt"
+
+    local languages="py js"
+    for lang in $languages; do
+        local file_extension
+        case $lang in
+            "py") file_extension="*.py" ;;
+            "js") file_extension="*.js" ;;
+            *) file_extension="*.*" ;;  # Default
+        esac
+
+        capitalized_lang=$(echo "$lang" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+        if [[ $(find "$folder_to_document" -type f -name "$file_extension") ]]; then
+            flow_map_file="$docs_folder/flow_map_${lang}.png"
+            generate_flow_map "$folder_to_document" "$flow_map_file" "$lang"
+            
+            echo -e "\n\n## Flow Map ($capitalized_lang)\n" >> "$docs_folder/High_Level_Design.md"
+            echo -e "![Flow Map ($capitalized_lang)](flow_map_${lang}.png)\n" >> "$docs_folder/High_Level_Design.md"
+        fi
+    done
 }
 
-######### Main script
-
-# Check if the required tools are installed
-check_tools
-
-# Get folder to document
-if [ $# -eq 0 ]; then
-  echo "Please provide a folder name as a command line argument"
-  exit 1
-fi
-
-folder=$1
-
-# Check if the folder exists
-if [ ! -d "$folder" ]; then
-  echo "Folder $folder does not exist"
-  exit 1
-fi
-
-# Populate the modules variable with subdirectories and files within the main directory, excluding node_modules and hidden directories
-modules=""
-while IFS= read -r -d '' module; do
-  if [[ -d $module || -f $module ]]; then
-    modules+="$module "
-  fi
-done < <(find "$folder" -type d -not -path '*/\.*' -not -path '*/node_modules*' -print0)
-
-echo -e "\n\n---- Modules to process: $modules ----\n\n"
-
-# Create the doc directory structure before processing the modules
-copy_dir_structure "$folder" "$folder"
-
-# Process subdirectories and files under the main directory
-IFS=$' ' # set space as delimiter
-for module in $modules; do
-  # For .js files within node_modules, skip the module
-  if [[ "$module" == *".js" ]] && is_skippable "$module"; then
-    continue
-  fi
-  echo -e "\n\n---- Processing module: $module ----"
-  generate_module_overview "$module" "doc_$module"
-done
-
-# Generate codebase overview
-# generate_codebase_overview
+main "$@"
