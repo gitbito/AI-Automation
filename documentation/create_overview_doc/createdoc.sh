b72c16df7f1d95020c86495d3dfe2c3dbe561eda
Troys-Code <TroyAndrewSchultz@gmail.com>
2023-10-27 18:14:23 -0700

    Refactored logging function, simplified document generation, and improved documentation script to better handle larger codebases and utilizes less bito calls for similar results
    - Refactored logging function to track token usage for a module, including input and output word and character counts, and improved log formatting.
    - Simplified document generation code to handle larger codebases without running out of context by reducing required prompt files, focused on individual module documentation feature extraction NLP technique extraction -> extrapolation, and removed functions for system overviews and combined contexts as this file tends to get too large for our current context window size of 100k.
    - Refactored documentation generation script to generate individual module documentation, aggregate them into 'Aggregated_High_Level_Doc.md,' and include flow maps and system summary. Removed redundant functions for module aggregation and design document generation.

--- documentation/create_overview_doc/createdoc.sh
+++ documentation/create_overview_doc/createdoc.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-log_file="bito_usage_log.txt" # Specify the log file here
-prompt_folder="AI_Prompts"  # Specify the prompt folder here
+log_file="bito_usage_log.txt" 
+prompt_folder="AI_Prompts"  
 
-function log_word_char_count() {
+function log_token_usage() {
     local module_name="$1"
-    local count_type="$2"
-    local word_count="$3"
-    local char_count="$4"
+    local input_content="$2"
+    local output_content="$3"
+
+    local input_word_count=$(echo "$input_content" | wc -w | tr -d ' ')
+    local output_word_count=$(echo "$output_content" | wc -w | tr -d ' ')
+    local total_word_count=$(( input_word_count + output_word_count ))
+
+    local input_char_count=$(echo "$input_content" | wc -c | tr -d ' ')
+    local output_char_count=$(echo "$output_content" | wc -c | tr -d ' ')
+    local total_char_count=$(( input_char_count + output_char_count ))
+
     local timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-    echo "$timestamp - Module '$module_name' Count $count_type BitO CLI: Words: $word_count, Chars: $char_count" | tee -a "$log_file"
+    echo "-----------------------------------------" | tee -a "$log_file"
+    echo "$timestamp - Token Usage for Module: '$module_name'" | tee -a "$log_file"
+    echo "Input: Words = $input_word_count, Chars = $input_char_count" | tee -a "$log_file"
+    echo "Output: Words = $output_word_count, Chars = $output_char_count" | tee -a "$log_file"
+    echo "Total: Words = $total_word_count, Chars = $total_char_count" | tee -a "$log_file"
+    echo "-----------------------------------------" | tee -a "$log_file"
 }
 
 function check_tools_and_files() {
@@ -22,7 +35,7 @@ function check_tools_and_files() {
         fi
     done
 
-    local required_files=("high_level_docstrings_prompt.txt" "system_summary_prompt.txt" "refined_organized_markdown_prompt.txt" )
+    local required_files=("high_level_doc_prompt.txt")
     for file in "${required_files[@]}"; do
         if [ ! -f "$prompt_folder/$file" ]; then
             echo "Error: Missing required file: $prompt_folder/$file"
@@ -42,9 +55,8 @@ function check_tools_and_files() {
 
 is_skippable() {
   local path=$1
-  
   local skip_dirs_files=("logs" "node_modules" "dist" "target" "bin" "package-lock.json" "data.json" "build" ".gradle" ".idea" "gradle" "extension.js" "vendor.js" "ngsw.json" "polyfills.js" "init")
-
+  
   for skip_item in "${skip_dirs_files[@]}"; do
     if [[ "$path" == *"$skip_item"* ]]; then
       return 0
@@ -58,8 +70,9 @@ is_skippable() {
   return 1
 }
 
-function aggregate_module_high_level_docstrings() {
+function generate_individual_module_md() {
     local module_path="$1"
+    local docs_folder="$2"
     
     if is_skippable "$module_path"; then
         echo "Skipped $module_path as it's in the skippable list."
@@ -67,94 +80,41 @@ function aggregate_module_high_level_docstrings() {
     fi
     
     local module_name=$(basename "$module_path")
-    local module_contents
+    local module_contents=$(<"$module_path")
 
-    module_contents=$(<"$module_path")
+    local high_level_doc=$(echo -e "Module: $module_name\n---\n$module_contents" | bito -p "$prompt_folder/high_level_doc_prompt.txt")
 
-    local high_level_docstrings=$(echo -e "Module: $module_name\n---\n$module_contents" | bito -p "$prompt_folder/high_level_docstrings_prompt.txt")
+    # Log token usage for both input and output
+    log_token_usage "$module_name" "$module_contents" "$high_level_doc"
 
     if [ $? -eq 0 ]; then
-        echo "$high_level_docstrings"
+        local module_md_file="$docs_folder/${module_name}_High_Level_Doc.md"
+        echo -e "## Module: $module_name\n$high_level_doc" > "$module_md_file"
+        echo "Saved module doc to $module_md_file"
     else
         echo "Bito operation failed for module: $module_name"
         return 1
     fi
 }
 
-function generate_system_overview() {
-    local aggregated_docstrings_file="$1"
-    local aggregated_docstrings=$(cat "$aggregated_docstrings_file")
-    local system_overview=$(echo -e "Aggregated Docstrings:\n---\n$aggregated_docstrings" | bito -p "$prompt_folder/system_summary_prompt.txt")
-
-    if [ $? -eq 0 ]; then
-        echo "$system_overview"
-    else
-        echo "Bito operation failed while generating system overview"
-        return 1
-    fi
-}
-
-function create_combined_context() {
-    local aggregated_module_docstrings_file="$1"
-    local system_overview_file="$2"
-    local combined_context_file="$docs_folder/combined_context.txt"
-    
-    # Use 'cat' to concatenate files efficiently
-    cat "$aggregated_module_docstrings_file" "$system_overview_file" > "$combined_context_file"
-
-    echo "Combined context generated and saved in combined_context.txt"
-}
-
-function generate_refined_design_doc() {
-    local input_file="$docs_folder/combined_context.txt"
-    local output_file="$docs_folder/High_Level_Design.md"
-
-    # Read the content of combined_context.txt
-    local combined_content=$(<"$input_file")
-
-    # Read prompt
-    local prompt=$(<"$prompt_folder/refined_organized_markdown_prompt.txt")
-
-    # Prepend the content of combined_context.txt to the prompt
-    local combined_prompt="${combined_content}\n${prompt}"
-
-    # Call BitO with combined prompt
-    local refined_doc=$(echo -e "$combined_prompt" | bito)
-
-    # Check if bito succeeded or failed
-    if [ $? -ne 0 ]; then
-        echo "Bito operation failed while generating refined design doc"
-        return 1
-    fi
-
-    # Write refined doc to output file
-    echo "$refined_doc" > "$output_file"
-}
-
 generate_flow_map() {
-    local module=$1
+    local folder_to_document=$1
     local flow_map_file=$2
     local lang_option=$3
 
-    echo -e "\n---- Generating code flow map for files in $module using language: $lang_option ----"
-
     local file_extension
     case $lang_option in
-        "py") file_extension="*.py" ;;      # Python
-        "js") file_extension="*.js" ;;      # JavaScript
-        "ruby") file_extension="*.rb" ;;    # Ruby
-        "php") file_extension="*.php" ;;    # PHP
-        # Add other cases as needed
-        *) file_extension="*.*" ;;  # Default
+        "py") file_extension="*.py" ;;      
+        "js") file_extension="*.js" ;;      
+        "ruby") file_extension="*.rb" ;;    
+        "php") file_extension="*.php" ;;    
+        *) file_extension="*.*" ;;  
     esac
 
-    local files_to_process=($(find "$module" -type f -name "$file_extension"))
-
-    # Debugging step to print the files you're about to process
-    echo "Files to process with code2flow: ${files_to_process[@]}"
+    local files_to_process=($(find "$folder_to_document" -type f -name "$file_extension"))
 
     if [[ ${#files_to_process[@]} -eq 0 ]]; then
-        echo "Warning: No files found for the language: $lang_option in $module"
+        echo "Warning: No files found for the language: $lang_option in $folder_to_document"
         return 1
     fi
 
@@ -171,7 +131,7 @@ function main() {
 
     folder_to_document="$1"
     docs_folder="doc_"$(basename "$folder_to_document")
-    
+
     if [ ! -d "$folder_to_document" ]; then
         echo "Folder $folder_to_document does not exist"
         exit 1
@@ -181,85 +141,48 @@ function main() {
         mkdir "$docs_folder"
     fi
 
-    aggregated_module_docstrings_file="$docs_folder/aggregated_module_docstrings.txt"
-    touch "$aggregated_module_docstrings_file"
-    
-    # For Python, C, C++, Java, JavaScript, Go, Rust, Ruby, PHP
+    aggregated_md_file="$docs_folder/Aggregated_High_Level_Doc.md"
+    if [ -f "$aggregated_md_file" ]; then
+        > "$aggregated_md_file"
+    fi
+
     module_files=$(find "$folder_to_document" -type f \( -name '*.py' -o -name '*.c' -o -name '*.cpp' -o -name '*.java' -o -name '*.js' -o -name '*.go' -o -name '*.rs' -o -name '*.rb' -o -name '*.php' \))
 
-    total_modules=0
     for module_file in $module_files; do
-        if ! is_skippable "$module_file"; then
-            ((total_modules++))
+        generate_individual_module_md "$module_file" "$docs_folder"
+    done
+    
+    # Aggregate individual md files
+    echo "# Full System Overview" > "$aggregated_md_file" 
+    for md_file in "$docs_folder"/*_High_Level_Doc.md; do
+        if [ "$md_file" != "$aggregated_md_file" ]; then
+            cat "$md_file" >> "$aggregated_md_file"
         fi
     done
-
-    aggregated_module_count=0
-    running_word_count=0
-    running_char_count=0
-    divider="=========================================================="
-
-    for module_file in $module_files; do
-        if is_skippable "$module_file"; then
-            continue  # Skip the current iteration and move to the next file
-        fi
-
-        module_summary=$(aggregate_module_high_level_docstrings "$module_file")
-
-        if [ $? -ne 0 ]; then
-            echo "Exiting due to Bito operation failure."
-            exit 1
-        fi
-
-        ((aggregated_module_count++))
-
-        word_count=$(echo -e "$module_summary" | wc -w)
-        char_count=$(echo -n "$module_summary" | wc -c)
-
-        module_filename=$(basename "$module_file")
-
-        running_word_count=$((running_word_count + word_count))
-        running_char_count=$((running_char_count + char_count))
-
-        echo "$divider"
-        echo "Aggregated '$module_filename' (Words: $word_count, Chars: $char_count)"
-        echo "$aggregated_module_count out of $total_modules modules"
-        echo "Total Word Count: $running_word_count, Total Char Count: $running_char_count"
-        echo "$divider"
-
-        echo -e "$module_summary\n" >> "$aggregated_module_docstrings_file"
-    done
-
-    system_overview_file="$docs_folder/system_overview.txt"
-    system_overview=$(generate_system_overview "$aggregated_module_docstrings_file")
-    echo "$system_overview" > "$system_overview_file"
-
-    create_combined_context "$aggregated_module_docstrings_file" "$system_overview_file"
-
-    generate_refined_design_doc "$docs_folder/combined_context.txt"
 
     local languages="py js ruby php"
-
     for lang in $languages; do
-        local file_extension
-        case $lang in
-            "py") file_extension="*.py" ;;     # Python
-            "js") file_extension="*.js" ;;     # JavaScript
-            "ruby") file_extension="*.rb" ;;   # Ruby
-            "php") file_extension="*.php" ;;   # PHP
-            # Add other cases as needed
-            *) file_extension="*.*" ;;  # Default
-        esac
+        local flow_map_file="$docs_folder/flow_map_${lang}.png"
+        local flow_map_gv_file="$docs_folder/flow_map_${lang}.gv"
+        generate_flow_map "$folder_to_document" "$flow_map_gv_file" "$lang"
+        if [ -f "$flow_map_gv_file" ]; then
+            # Generate flow map PNG file from .gv file
+            dot -Tpng "$flow_map_gv_file" -o "$flow_map_file"
 
-        capitalized_lang=$(echo "$lang" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
-        if [[ $(find "$folder_to_document" -type f -name "$file_extension") ]]; then
-            flow_map_file="$docs_folder/flow_map_${lang}.png"
-            generate_flow_map "$folder_to_document" "$flow_map_file" "$lang"
+            echo -e "\n\n## Flow Map ($lang)\n" >> "$aggregated_md_file"
+            echo -e "![Flow Map ($lang)](flow_map_${lang}.png)\n" >> "$aggregated_md_file"
             
-            echo -e "\n\n## Flow Map ($capitalized_lang)\n" >> "$docs_folder/High_Level_Design.md"
-            echo -e "![Flow Map ($capitalized_lang)](flow_map_${lang}.png)\n" >> "$docs_folder/High_Level_Design.md"
+            # Perform Bito operation for system summary and append to aggregated_md_file
+            system_summary_prompt="$prompt_folder/system_summary_prompt.txt"
+            bito_output=$(cat "$system_summary_prompt" "$flow_map_gv_file" | bito -p "$system_summary_prompt")
+
+            # Append the Bito output to the aggregated high-level documentation file
+            echo -e "\n\n## System Summary\n" >> "$aggregated_md_file"
+            echo -e "$bito_output" >> "$aggregated_md_file"
         fi
     done
+
+    echo "Documentation generated in $docs_folder"
 }
 
 main "$@"
